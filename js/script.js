ab5cec75ff471fc8bfef427dadf1d9aad9a7bced
maxhakka <hakka@kth.se>
2020-06-06 21:17:35 +0200

    Added fab button to scroll to top of page

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
+
+function displayFabButton() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+mybutton = document.getElementById("scrollButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {displayFabButton()};
