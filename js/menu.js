9564b2e7c0b0b2a2bcf2d30b086405fb47ac3df5
maxhakka <hakka@kth.se>
2020-06-12 22:16:42 +0200

    Updated styles

--- js/menu.js
+++ js/menu.js
@@ -2,19 +2,19 @@
 let targetOffset = $("#about").offset().top;
 let secondTargetOffset = $("#contact").offset().top;
 
-$('#prtf').css({"color": "rgb(111, 0, 255)"});
+$('#prtf').css({"color": "rgb(29, 63, 255)"});
 
 var $w = $(window).scroll(function(){
     if ( $w.scrollTop() >= secondTargetOffset - 50 ) {
       $('#prtf').css({"color":"black"});
       $('#abt').css({"color":"black"});
-      $('#cntct').css({"color":"rgb(111, 0, 255)"});
+      $('#cntct').css({"color":"rgb(29, 63, 255)"});
     } else if ( $w.scrollTop() >= targetOffset - 50){
       $('#prtf').css({"color":"black"});
-      $('#abt').css({"color":"rgb(111, 0, 255)"});
+      $('#abt').css({"color":"rgb(29, 63, 255)"});
       $('#cntct').css({"color":"black"});
     } else {
-      $('#prtf').css({"color":"rgb(111, 0, 255)"});
+      $('#prtf').css({"color":"rgb(29, 63, 255)"});
       $('#abt').css({"color":"black"});
       $('#cntct').css({"color":"black"});
     }
