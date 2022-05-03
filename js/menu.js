9ae093f89c8d1e2ce2fc97452a2614217bad1581
maxhakka <hakka@kth.se>
2022-05-03 17:13:29 +0200

    Updated texts
    Updated descriptions
    Updated text colors

--- js/menu.js
+++ js/menu.js
@@ -6,16 +6,16 @@ $('#prtf').css({"color": "rgb(29, 63, 255)"});
 
 var $w = $(window).scroll(function(){
     if ( $w.scrollTop() >= secondTargetOffset - 50 ) {
-      $('#prtf').css({"color":"black"});
-      $('#abt').css({"color":"black"});
+      $('#prtf').css({"color":"rgba(0, 0, 0, 0.8);"});
+      $('#abt').css({"color":"rgba(0, 0, 0, 0.8);"});
       $('#cntct').css({"color":"rgb(29, 63, 255)"});
     } else if ( $w.scrollTop() >= targetOffset - 50){
-      $('#prtf').css({"color":"black"});
+      $('#prtf').css({"color":"rgba(0, 0, 0, 0.8);"});
       $('#abt').css({"color":"rgb(29, 63, 255)"});
-      $('#cntct').css({"color":"black"});
+      $('#cntct').css({"color":"rgba(0, 0, 0, 0.8);"});
     } else {
       $('#prtf').css({"color":"rgb(29, 63, 255)"});
-      $('#abt').css({"color":"black"});
-      $('#cntct').css({"color":"black"});
+      $('#abt').css({"color":"rgba(0, 0, 0, 0.8);"});
+      $('#cntct').css({"color":"rgba(0, 0, 0, 0.8);"});
     }
 });
